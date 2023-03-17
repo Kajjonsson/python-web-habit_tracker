62a8a772bc7d685ba4829b9595d04522f29bfee4
Kaj Jonsson <kajjonsson89@gmail.com>
2023-03-17 13:54:14 +0100

    habit-tracker

--- app.py
+++ app.py
@@ -1,49 +1,18 @@
-"""Backend for web app habit tracker"""
-import datetime
-from collections import defaultdict
-from flask import Flask, render_template, request, redirect, url_for
+"""Registering the flask blueprints for the web app."""
+import os
+from flask import Flask
+from routes import pages
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
-app = Flask(__name__)
+load_dotenv()
 
-habits = ["Test habit"]
-completions = defaultdict(list)
+def create_app():
+    """Factory for creating the app"""
+    app = Flask(__name__)
+    client = MongoClient(os.environ.get("MONGODB_URI"))
+    app.db = client.get_default_database()
+    
+    app.register_blueprint(pages)
+    return app
 
-@app.context_processor
-def add_calc_date_range():
-    def date_range(start: datetime.date):
-        dates = [start + datetime.timedelta(days=diff) for diff in range(-3,4)]
-        return dates
-
-    return {"date_range": date_range}
-
-@app.route("/")
-def index():
-    date_str = request.args.get("date")
-    if date_str:
-        selected_date = datetime.date.fromisoformat(date_str)
-    else:
-        selected_date = datetime.date.today()
-    return render_template(
-        "index.html",
-        habits=habits,
-        selected_date=selected_date,
-        completions=completions[selected_date],
-        title="Habit Tracker - Home",
-        )
-
-@app.route("/add", methods=["GET", "POST"])
-def add_habit():
-    if request.method == "POST":
-        habit = request.form.get("habit")
-        habits.append(habit)
-    return render_template("add_habit.html", title="Habit Tracker - Add Habit", selected_date=datetime.date.today(),
-)
-
-@app.route("/complete", methods=["POST"])
-def complete():
-    date_string = request.form.get("date")
-    date = datetime.date.fromisoformat(date_string)
-    habit = request.form.get("habitName")
-    completions[date].append(habit)
-
-    return redirect(url_for("index", date=date_string))
